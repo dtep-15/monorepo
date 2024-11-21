0ff3140f06769398f17c884ecfeeac04c2631cd7
hoanghuudao <huudao2536@gmail.com>
2024-11-21 22:53:43 +0200

    Connect & status APIs

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -33,7 +33,10 @@ const static esp_vfs_littlefs_conf_t LittleFSConfig{
 Config* config;
 
 std::string format_time(uint32_t time) {
-  return std::format("{:02}:{:02}:{:02}", time / (60 * 60), time / 60 % 60, time % 60);
+  uint8_t hours = time / (60 * 60);
+  uint8_t minutes = (time / 60) - (hours * 60);
+  uint8_t seconds = time % 60;
+  return std::format("{:02}:{:02}:{:02}", hours, minutes, seconds);
 }
 
 uint32_t parse_time(const std::string_view& time) {
@@ -57,45 +60,76 @@ extern "C" void app_main() {
   } else {
     WiFi::AP::start("Uninitialized Curtain Opener");
   }
-  Server::init(LittleFSConfig.base_path, {{"networks", HTTP_GET,
-                                           [](const auto req) {
-                                             Json::JsonDocument doc;
-                                             const auto networks = WiFi::STA::scan();
-                                             if (!networks.has_value()) {
-                                               return Server::API::Response(esp_err_to_name(networks.error()),
-                                                                            "500 Internal Server Error");
-                                             }
-                                             for (const auto& network : networks.value()) {
-                                               auto object = doc.add<Json::JsonObject>();
-                                               object["name"] = network.ssid;
-                                               object["password_protected"] = network.authmode != WIFI_AUTH_OPEN;
-                                             }
-                                             std::string result;
-                                             Json::serializeJson(doc, result);
-                                             return Server::API::Response(result, "200 OK");
-                                           }},
-                                          /* {"networks/connect", HTTP_POST, [](const auto req) {
-                                       JsonDocument doc;
-                                       if (Json::deserializeJson(doc, req.content)) {
-                                         return Server::API::Response("", 403);
-                                       }
-                                       if (doc[])
-                                      }}, */
-                                          {"schedule", HTTP_GET, [](const auto req) {
-                                              Json::JsonDocument doc;
-                                              doc["open_at"] = format_time(config->open_time);
-                                              doc["close_at"] = format_time(config->close_time);
-                                              std::string result;
-                                              Json::serializeJson(doc, result);
-                                              return Server::API::Response(result, "200 OK");
-                                            }}, {"schedule", HTTP_POST, [](const auto req) {
-                                              Json::JsonDocument doc;
-                                              Json::deserializeJson(doc, req.content);
-                                              config->open_time = parse_time(doc["open_at"] | "6:0:0");
-                                              config->close_time = parse_time(doc["close_at"] | "18:0:0");
-                                              config->save();
-                                              return Server::API::Response("", "200 OK");
-                                            }}});
+  // clang-format off
+  Server::init(LittleFSConfig.base_path, {
+  {"state", HTTP_GET, [](const auto req) {
+    Json::JsonDocument doc; 
+    if (config->ssid.has_value()) {
+        doc["is_configured"] = true;
+        doc["ssid"] = config->ssid.value();
+    } else {
+        doc["is_configured"] = false;
+        doc["ssid"] = nullptr;
+    }
+    std::string result;
+    Json::serializeJson(doc, result);
+    return Server::API::Response(result, "200 OK");
+  }},
+  {"networks", HTTP_GET,
+    [](const auto req) {
+      Json::JsonDocument doc;
+      const auto networks = WiFi::STA::scan();
+      if (!networks.has_value()) {
+        return Server::API::Response(esp_err_to_name(networks.error()),
+                                    "500 Internal Server Error");
+      }
+      for (const auto& network : networks.value()) {
+        auto object = doc.add<Json::JsonObject>();
+        object["name"] = network.ssid;
+        object["password_protected"] = network.authmode != WIFI_AUTH_OPEN;
+      }
+      std::string result;
+      Json::serializeJson(doc, result);
+      return Server::API::Response(result, "200 OK");
+    }},
+  {"networks/connect", HTTP_POST, [](const auto req) {
+    Json::JsonDocument doc;
+    if (Json::deserializeJson(doc, req.content)) {
+      return Server::API::Response("", "400 Bad Request");
+    }
+    std::string ssid = doc["name"] | "";
+    std::string password = doc["password"] | "";
+    if (ssid.empty()) {
+      return Server::API::Response("SSID is required", "400 Bad Request");
+    }
+    bool connected = true; 
+    // TODO: connect method to return status
+    WiFi::STA::connect(ssid, password);
+    if (!connected) {
+      return Server::API::Response("Failed to connect", "403 Forbidden");
+    }
+    config->ssid = ssid;
+    config->password = password;
+    config->save();
+    return Server::API::Response("", "200 OK");
+    }},
+  {"schedule", HTTP_GET, [](const auto req) {
+      Json::JsonDocument doc;
+      doc["open_at"] = format_time(config->open_time);
+      doc["close_at"] = format_time(config->close_time);
+      std::string result;
+      Json::serializeJson(doc, result);
+      return Server::API::Response(result, "200 OK");
+    }}, {"schedule", HTTP_POST, [](const auto req) {
+      Json::JsonDocument doc;
+      Json::deserializeJson(doc, req.content);
+      config->open_time = parse_time(doc["open_at"] | "6:0:0");
+      config->close_time = parse_time(doc["close_at"] | "18:0:0");
+      config->save();
+      return Server::API::Response("", "200 OK");
+    }}}
+  );
+  // clang-format on 
   mdns_init();
   mdns_hostname_set("curtain");
   // Curtain::init({.gpio_num = 14, .resolution=1'000'000, .period_ticks=20'000});
